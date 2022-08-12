1ba842b58d4b70ccf5d61d553e8900c84a7c5159
jibdrt <jb.druet@codeur.online>
2022-08-12 17:42:54 +0200

    find by Id with params.id

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -27,7 +27,7 @@ exports.getAllNotes = (req, res) => {
 };
 
 exports.getOneNote = (req, res) => {
-    Note.find({ _id: "62f66d473243abad5f7e5c1d" }).exec((err, note) => {
+    Note.find({ _id: req.params.id }).exec((err, note) => {
         if (err) {
             res.status(500).send({ message: err });
             return;
--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -3,5 +3,5 @@ const controller = require("../controllers/note.controller");
 module.exports = function(app) {
     app.post("/api/notes", controller.postnotes);
     app.get("/api/notes", controller.getAllNotes);
-    app.get("/api/notes/one", controller.getOneNote);
+    app.get("/api/notes/:id", controller.getOneNote);
 }
