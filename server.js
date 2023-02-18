6a690ea1cc4bc35ca1fd07a9687781659038dbc0
jibdrt <jb.druet@codeur.online>
2023-02-18 12:45:57 +0100

    init two roles and default admin account at launch

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get("/", (req, res) => {
 
 const db = require("./app/models");
 const Role = db.role;
+const User = db.user;
 
-console.log(dbConfig);
 db.mongoose
   .connect(`mongodb://${process.env.DBHOST}:${process.env.DBPORT}/${process.env.DB}`, {
     useNewUrlParser: true,
@@ -40,7 +40,15 @@ db.mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    initial();
+    setTimeout(() => {
+      initial()
+        .then(() => {
+          console.log("Initialization completed");
+        })
+        .catch((error) => {
+          console.log("Initialization error: ", error);
+        });
+    }, 1000);                                         // Retarder l'exécution de initial de 1 sec
   })
   .catch(err => {
     console.error("Connection error", err);
@@ -48,28 +56,88 @@ db.mongoose
   });
 
 
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
+  function initial() {
+    return new Promise((resolve, reject) => {
+      Role.estimatedDocumentCount((err, count) => {
         if (err) {
-          console.log("error", err);
+          console.error("Error counting roles:", err);
+          reject(err);
+        } else {
+          if (count === 2) {
+            let adminRole;
+            Role.findOne({ name: "admin" })
+              .then((role) => {
+                if (role) {
+                  adminRole = role;
+                  console.log("Admin role found:", role);
+                } else {
+                  console.log("Admin role not found, creating...");
+                  return new Role({
+                    name: "admin"
+                  }).save();
+                }
+              })
+              .then((createdRole) => {
+                adminRole = createdRole || adminRole; // utilise le rôle créé ou celui trouvé
+                console.log("Using admin role:", adminRole);
+                return Promise.all([
+                  new Role({
+                    name: "user"
+                  }).save(),
+                  new User({
+                    name: "admino",
+                    password: "admino",
+                    email: "admino@gmail.com",
+                    roles: [adminRole._id] // utilise l'_id de l'adminRole
+                  }).save()
+                ]);
+              })
+              .then(() => {
+                console.log("Added roles and user");
+                resolve();
+              })
+              .catch((error) => {
+                console.error("Error adding roles and user:", error);
+                reject(error);
+              });
+          } else {
+            console.log("Number of roles in database:", count);
+            console.log("No need to add roles and user");
+            resolve();
+          }
         }
-        console.log("added user to roles");
       });
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added admin to roles");
-      });
-    }
-  });
-};
+    });
+  }
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+/* function findAdminRoleId() {
+  return Role.findOne({ name: "admin" })
+    .then((role) => {
+      if (role) {
+        console.log("Found admin role:", role);
+        return role._id;
+      } else {
+        console.log("Admin role not found");
+        return null;
+      }
+    })
+    .catch((error) => {
+      console.error("Error finding admin role:", error);
+      return null;
+    });
+} */
+
+
 
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
