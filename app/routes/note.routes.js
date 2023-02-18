6262b00fe73a847dd08eb0522f21a0ad3602764a
jibdrt <jb.druet@codeur.online>
2023-02-18 17:19:29 +0100

    update route notes/me

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/note.controller");
 module.exports = function (app) {
  
     app.get("/api/notes", controller.getAllNotes);
+    app.get("/api/notes/me", [authJwt.verifyToken], controller.getMyNotes);
     app.get("/api/notes/:id", controller.getOneNote);
     app.post("/api/notes", [authJwt.verifyToken], controller.newNote);
     app.patch("/api/notes/:id", controller.updateOneNote);
